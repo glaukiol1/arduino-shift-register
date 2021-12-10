fb7a604c3f3a115d95039d1c70d0a59a62897bd5
glaukiol1 <glaukiol1@gmail.com>
2021-12-10 16:15:39 +0100

    first commit

---
+++ shift_registors.ino
@@ -0,0 +1,41 @@
+// ST_CP pin 12
+const int latchPin = 10;
+// SH_CP pin 11
+const int clockPin = 11;
+// DS pin 14
+const int dataPin = 12;
+ 
+void setup ()
+{
+  // Setup pins as Outputs
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+}
+
+byte bin = 0b00000000;
+
+void shiftWrite(int pin, int val) {
+  if (val == 0) {
+    bitClear(bin, pin);
+  }
+
+  if (val == 1) {
+    bitSet(bin, pin);
+  }
+  refreshShift();
+}
+
+void setBin(byte newBin) {
+  bin = newBin;
+  refreshShift();
+}
+
+void refreshShift() {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, bin);
+  digitalWrite(latchPin, HIGH);
+}
+void loop() {
+  setBin(0b1100);
+}
